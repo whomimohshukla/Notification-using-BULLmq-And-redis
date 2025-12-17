f46c08d075796582bea119e76553c5c5db368ee4
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-17 12:00:07 +0530

    format code for consistency in dlq queue configuration

--- src/queue/dlq.queue.ts
+++ src/queue/dlq.queue.ts
@@ -1,17 +1,16 @@
 import { Queue } from "bullmq";
 
-import {redis} from "../config/redis";
-
+import { redis } from "../config/redis";
 
 export const dlqQueue = new Queue("notificationDLQ", {
-    connection: redis,
+	connection: redis,
 
-    defaultJobOptions: {
-        attempts: 3,
-        backoff: {
-            type: "exponential",
-            delay: 1000,
-        },
-        removeOnComplete: true,
-    },
+	defaultJobOptions: {
+		attempts: 3,
+		backoff: {
+			type: "exponential",
+			delay: 1000,
+		},
+		removeOnComplete: true,
+	},
 });
