ab6002bfdf96ac152face803bcbdd7be369fc4bf
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-17 12:00:22 +0530

    format code for consistency in notification queue configuration

--- src/queue/notification.queue.ts
+++ src/queue/notification.queue.ts
@@ -2,17 +2,15 @@ import { Queue } from "bullmq";
 
 import { redis } from "../config/redis";
 
-
-
 export const notificationQueue = new Queue("notificationQueue", {
-    connection: redis,
+	connection: redis,
 
-    defaultJobOptions: {
-        attempts: 3,
-        backoff: {
-            type: "exponential",
-            delay: 1000,
-        },
-        removeOnComplete: true,
-    },
+	defaultJobOptions: {
+		attempts: 3,
+		backoff: {
+			type: "exponential",
+			delay: 1000,
+		},
+		removeOnComplete: true,
+	},
 });
